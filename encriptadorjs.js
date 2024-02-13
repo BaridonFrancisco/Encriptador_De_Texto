4f3f94d29f7f3e34100a6f07de9b1c0108511dcb
BaridonFrancisco <baridonfrancisco@gmail.com>
2024-02-12 23:48:50 -0300

    agregando funcionalidades a los botones

--- encriptadorjs.js
+++ encriptadorjs.js
@@ -18,3 +18,40 @@ textarea[1].addEventListener("input",(e) => {
     }
 });
 
+botonEncritar.addEventListener('click',(e)=>{
+        let valor=textarea[0].value;
+        if(valor!=''){
+            const arr=valor.split('');
+            arr.forEach(element => {
+                console.log(element);
+            });
+        }
+        
+});
+
+let conversion=function name(arr) {
+    let cadena='';
+   arr.forEach(element => {
+        switch (element) {
+            case 'a':
+                cadena+='ai';
+                break;
+            case 'e':
+                cadena+='enter';
+                break
+            case 'i':
+                cadena+='imes';
+            case 'o':
+                cadena+='ober';    
+                break;
+            case 'u':
+                cadena+='ufat';
+                break;    
+            default:
+                cadena+=element;
+                break;
+        }
+   });
+   return cadena;
+}
+
